5ec2cb578041e04de66954f6aeeab284f11f70a3
Jacques741 <jacquesccmoll@gmail.com>
2023-09-15 14:54:31 +0200

    Created all Interface and normal repository classes for each controller

---
+++ EcoPower_Logistics/SuperStore P3/SuperStore P3/Repository/CustomerRepository.cs
@@ -0,0 +1,10 @@
+﻿using Data;
+using Models;
+
+namespace EcoPower_Logistics.Repository
+{
+    public class CustomerRepository : GenericRepository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(SuperStoreContext context) : base(context) { }
+    }
+}
---
+++ EcoPower_Logistics/SuperStore P3/SuperStore P3/Repository/OrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using Models;
+using Data;
+
+namespace EcoPower_Logistics.Repository
+{
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(SuperStoreContext context) : base(context) { }
+    }
+}
